81ce2e13358cf5d5244f2a71e10cc093435562c2
Jayden Wojcik <jm12wojc@siena.edu>
2024-02-18 22:06:52 -0500

    Added click to make fullscreen to gallery

---
+++ final1/gallery.js
@@ -0,0 +1,15 @@
+document.querySelectorAll(".photo-container img").forEach((element) => {
+    let parent = element.parentElement;
+    let temp = document.getElementById("photoInsert");
+
+    element.addEventListener("click", () => {
+        temp.append(element);
+        element.classList.add("photoInsertImg")
+        temp.classList.add("photoInsertBlurred");
+        temp.addEventListener("click", () => {
+            element.classList.remove("photoInsertImg")
+            temp.classList.remove("photoInsertBlurred");
+            parent.append(element);
+        });
+    });
+});
