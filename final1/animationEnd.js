9ff25e7fdadf71e1533dc7532253d6c3b46b7479
Jayden Wojcik <jm12wojc@siena.edu>
2024-02-27 23:12:32 -0500

    Complete Project 2

--- final1/animationEnd.js
+++ final1/animationEnd.js
@@ -1,15 +1,18 @@
-let elems = document.querySelectorAll(".bento > div");
-var index = 0,
-  length = elems.length;
-for (; index < length; index++) {
-  let element = elems[index];
-  let anims = element.getAnimations();
-  anims.forEach(async (anim) => {
-    function onFinish() {
-      anim.cancel();
-      element.classList.add("scaled100");
-    }
-    if (!anim.startTime || !anim.finished) anim.addEventListener("finish", onFinish);
-    else onFinish();
-  });
-}
+document.addEventListener("DOMContentLoaded", () => {
+  let elems = document.querySelectorAll(".bento > div");
+  var index = 0,
+    length = elems.length;
+  for (; index < length; index++) {
+    let element = elems[index];
+    let anims = element.getAnimations();
+    anims.forEach(async (anim) => {
+      function onFinish() {
+        anim.cancel();
+        element.classList.add("scaled100");
+      }
+      if (!anim.startTime || !anim.finished)
+        anim.addEventListener("finish", onFinish);
+      else onFinish();
+    });
+  }
+});
--- final1/gallery.js
+++ final1/gallery.js
@@ -1,15 +1,17 @@
-document.querySelectorAll(".photo-container img").forEach((element) => {
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".photo-container img").forEach((element) => {
     let parent = element.parentElement;
     let temp = document.getElementById("photoInsert");
 
     element.addEventListener("click", () => {
-        temp.append(element);
-        element.classList.add("photoInsertImg")
-        temp.classList.add("photoInsertBlurred");
-        temp.addEventListener("click", () => {
-            element.classList.remove("photoInsertImg")
-            temp.classList.remove("photoInsertBlurred");
-            parent.append(element);
-        });
+      temp.append(element);
+      element.classList.add("photoInsertImg");
+      temp.classList.add("photoInsertBlurred");
+      temp.addEventListener("click", () => {
+        element.classList.remove("photoInsertImg");
+        temp.classList.remove("photoInsertBlurred");
+        parent.append(element);
+      });
     });
+  });
 });
